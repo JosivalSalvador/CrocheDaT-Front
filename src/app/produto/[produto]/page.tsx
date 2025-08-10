5f5dc0cb14fb9d7e0d2970c64b9a45c5bc4240d2
Josival Salvador <josivaljunior087@gmail.com>
2025-08-10 18:11:27 -0400

    Ajustando descrição

--- src/app/produto/[produto]/page.tsx
+++ src/app/produto/[produto]/page.tsx
@@ -91,7 +91,7 @@ export default function Produto() {
 
                 <ul className="list-group list-group-flush mb-3">
                   <li className="list-group-item">
-                    <strong>Descrição:</strong> {(produto.description || "").replace(/\([^)]*\)/g, "").trim() || "Sem descrição"}
+                    <strong>Descrição:</strong> {(produto.description || "").replace(/\[[^\]]*\]/g, "").trim() || "Sem descrição"}
                   </li>
                   <li className="list-group-item">
                     <strong>Material:</strong> {produto.material}
@@ -103,7 +103,7 @@ export default function Produto() {
                     <strong>Categoria:</strong> {produto.category?.name || "Sem categoria"}
                   </li>
                   <li className="list-group-item">
-                    <strong>Observações:</strong> {((produto.description || "").match(/\(([^)]*)\)/g) || []).map(obs => obs.replace(/[()]/g, "").trim()).join(". ") || "Sem observações"}
+                    <strong>Observações:</strong> {((produto.description || "").match(/\[([^\]]*)\]/g) || []).map(obs => obs.replace(/[\[\]]/g, "").trim()).join(". ") || "Sem observações"}
                   </li>
                 </ul>
 
