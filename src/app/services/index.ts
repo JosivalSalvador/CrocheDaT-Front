5c13cfbf3cb5a307ad7c96a15efdac21a1b46390
Josival Salvador <josivaljunior087@gmail.com>
2025-08-19 06:53:39 -0400

    Ajustando api

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -2,5 +2,6 @@ import axios from "axios";
 
 
 export const produtosApi = axios.create({
-    baseURL: "https://crochedat-back.onrender.com/v1",
+    baseURL: "https://crochedat.up.railway.app/v1",
+    /*baseURL: "https://crochedat-back.onrender.com/v1",*/
 });
